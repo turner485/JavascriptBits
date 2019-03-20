4c0318fb5a79af589f34633d3d40ff297f0f3930
Ben Turner <Ben.Turner@joules.com>
2019-03-20 15:52:42 +0000

    prettier code

--- Functions/script.js
+++ Functions/script.js
@@ -1,25 +1,25 @@
 // defining a functions
 
 const square = function(x) {
-  return x * x;
+    return x * x;
 };
 
 console.log(square(12));
 // → 144
 
 const makeNoise = function() {
-  console.log("Pling!");
+    console.log("Pling!");
 };
 
 makeNoise();
 // → Pling!
 
 const power = function(base, exponent) {
-  let result = 1;
-  for (let count = 0; count < exponent; count++) {
-    result *= base;
-  }
-  return result;
+    let result = 1;
+    for (let count = 0; count < exponent; count++) {
+        result *= base;
+    }
+    return result;
 };
 
 console.log(power(2, 10));
@@ -28,10 +28,10 @@ console.log(power(2, 10));
 // Bindings and scopes
 let x = 10;
 if (true) {
-  let y = 20;
-  var z = 30;
-  console.log(x + y + z);
-  // → 60
+    let y = 20;
+    var z = 30;
+    console.log(x + y + z);
+    // → 60
 }
 
 // y is not visible here
@@ -39,7 +39,7 @@ console.log(x + z);
 // → 40
 
 const halve = function(n) {
-  return n / 2;
+    return n / 2;
 };
 
 let n = 10;
@@ -51,31 +51,31 @@ console.log(n);
 // Functions as values
 
 let launchMissiles = function() {
-  missileSystem.launch("now");
+    missileSystem.launch("now");
 };
 if (safeMode) {
-  launchMissiles = function() {
-    /* do nothing */
-  };
+    launchMissiles = function() {
+        /* do nothing */
+    };
 }
 
 // Arrow functions
 // multiple value
 const power = (base, exponent) => {
-  let result = 1;
-  for (let count = 0; count < exponent; count++) {
-    result *= base;
-  }
-  return result;
+    let result = 1;
+    for (let count = 0; count < exponent; count++) {
+        result *= base;
+    }
+    return result;
 };
 
 // single value
 const square1 = x => {
-  return x * x;
+    return x * x;
 };
 const square2 = x => x * x;
 
 // empty perenthasis
 const horn = () => {
-  console.log("Toot");
+    console.log("Toot");
 };
